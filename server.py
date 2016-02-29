d4c2dee847b4b1ea68201f8cb22575db45f4bcbe
Yufei Zhang <yufeiGitHub@gmail.com>
2016-02-28 22:03:56 -0700

    update server.py

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 import json
 app = Flask(__name__)
 app.debug = True
@@ -79,22 +79,28 @@ def hello():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    jsonData = flask_post_json()
+    for key in jsonData.keys():
+        myWorld.update(entity,key,jsonData[key])
+
+    return get_entity(entity)
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.world());
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return world()
 
 if __name__ == "__main__":
     app.run()
